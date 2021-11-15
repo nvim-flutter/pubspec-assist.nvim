612be122c3790df2a274c8d3649057ce2f90e781
Akin Sowemimo <akin@email.com>
2021-11-15 13:32:14 +0000

    feat: add semver package for up to dateness check

--- lua/pubspec-assist.lua
+++ lua/pubspec-assist.lua
@@ -7,12 +7,26 @@ local fmt = string.format
 local NAMESPACE = api.nvim_create_namespace("pubspec_assist")
 local BASE_URI = "https://pub.dartlang.org/api"
 local PUBSPEC_FILE = "pubspec.yaml"
+local HL_PREFIX = "PubspecAssist"
+local hls = {
+  OUTDATED = HL_PREFIX .. "DependencyOutdated",
+  UP_TO_DATE = HL_PREFIX .. "DependencyUpToDate",
+}
 
 ---@class PubspecAssistConfig
 
+---@class Package
+---@field current string
+---@field latest string
+---@field latest_published string
+---@field versions table[]
+---@field lnum number
+---@field error table
+
 local defaults = {
   highlights = {
-    dependency = "Comment",
+    up_to_date = "Comment",
+    outdated = "WarningMsg",
   },
 }
 
@@ -21,7 +35,8 @@ local defaults = {
 function M.setup(user_config)
   user_config = user_config or {}
   M.config = vim.tbl_deep_extend("force", defaults, user_config)
-  vim.cmd("highlight link PubspecAssistDependency " .. M.config.highlights.dependency)
+  vim.cmd(fmt("highlight link %s %s", hls.OUTDATED, M.config.highlights.outdated))
+  vim.cmd(fmt("highlight link %s %s", hls.UP_TO_DATE, M.config.highlights.up_to_date))
   vim.cmd(
     fmt(
       'autocmd! BufEnter %s lua require("pubspec-assist").show_dependency_versions()',
@@ -30,10 +45,23 @@ function M.setup(user_config)
   )
 end
 
-local function show_version(package)
+local semver = function(version)
+  local v = require("semver")
+  if not version or type(version) ~= "string" then
+    return nil
+  end
+  return v(version:gsub("%^", ""))
+end
+
+---Render the version text beside each line of the pubspec yaml
+---@param buf_id number
+---@param package Package
+local function show_version(buf_id, package)
   if package and package.latest and package.lnum and not package.error then
-    api.nvim_buf_set_extmark(0, NAMESPACE, package.lnum - 1, -1, {
-      virt_text = { { package.latest, "PubspecAssistDependency" } },
+    local current, latest = semver(package.current), semver(package.latest)
+    local hl = latest > current and hls.OUTDATED or hls.UP_TO_DATE
+    api.nvim_buf_set_extmark(buf_id, NAMESPACE, package.lnum - 1, -1, {
+      virt_text = { { package.latest, hl } },
       virt_text_pos = "eol",
       hl_mode = "combine",
     })
@@ -84,14 +112,16 @@ local function extract_dependency_info(dependency)
   return data
 end
 
-local function on_success(results, name, body)
-  assert(results, "The results must be passed in")
-  assert(name, "The package name must be passed in")
+local function on_success(context, body)
+  assert(context, "The context must be passed in")
   assert(body, "The response body must be passed in")
+  local results = context.results
+  local name = context.package_name
+  local buf_id = context.buf_id
   local dependency = vim.fn.json_decode(table.concat(body, "\n"))
   if results[name] then
     results[name] = vim.tbl_extend("force", results[name], extract_dependency_info(dependency))
-    show_version(results[name])
+    show_version(buf_id, results[name])
   end
 end
 
@@ -116,6 +146,7 @@ local function match_dependencies(dependencies, lines)
         local package_name = vim.trim(key:gsub(":", ""))
         if dependencies[package_name] then
           results[package_name] = results[package_name] or {}
+          results[package_name].current = dependencies[package_name]
           results[package_name].lnum = lnum
         end
       end
@@ -134,7 +165,6 @@ function M.show_dependency_versions()
   end, lines)
   local content = table.concat(filtered, "\n")
   local pubspec = require("yaml").eval(content)
-  local jobs = {}
   local dependencies = vim.tbl_extend(
     "keep",
     {},
@@ -142,10 +172,15 @@ function M.show_dependency_versions()
     pubspec.dev_dependencies or {}
   )
   local results = match_dependencies(dependencies, lines)
+  local buf_id = api.nvim_get_current_buf()
   for package, _ in pairs(dependencies) do
-    local err, success = wrap(on_err, results, package), wrap(on_success, results, package)
-    local job = fetch(fmt("packages/%s", package), err, success)
-    jobs[#jobs + 1] = job
+    local context = {
+      buf_id = buf_id,
+      results = results,
+      package_name = package,
+    }
+    local err, success = wrap(on_err, context), wrap(on_success, context)
+    fetch(fmt("packages/%s", package), err, success)
   end
 end
 
